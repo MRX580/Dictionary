345df3d0aca6366f9f7edca6c18448a51f072d8f
MRX580 <71222222+MRX580@users.noreply.github.com>
2022-08-01 21:42:25 +0200

    Test

--- create_bot.py
+++ create_bot.py
@@ -1,6 +1,11 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from config import API_TOKEN
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(find_dotenv())
+
 brain = MemoryStorage()
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=os.getenv('API_TOKEN'))
 dp = Dispatcher(bot, storage=brain)
---
+++ run.bat
@@ -0,0 +1,9 @@
+@echo off
+
+cd %CD%
+
+set API_TOKEN = '5210317786:AAHVmyvucnN0Gm1lPW-3_cn6kAX9YzDwuXs'
+
+python main.py
+
+pause
