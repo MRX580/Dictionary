3c1d8aa734480e32c783bb4a8f456ce8861c3a4f
MRX580 <71222222+MRX580@users.noreply.github.com>
2022-08-01 22:02:56 +0200

    Finish

--- create_bot.py
+++ create_bot.py
@@ -1,6 +1,5 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from config import API_TOKEN
 from dotenv import load_dotenv, find_dotenv
 import os
 
@@ -9,3 +8,4 @@ load_dotenv(find_dotenv())
 brain = MemoryStorage()
 bot = Bot(token=os.getenv('API_TOKEN'))
 dp = Dispatcher(bot, storage=brain)
+print("ONLINE")
